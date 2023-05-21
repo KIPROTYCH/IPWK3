6957b058886276464ff48d9820896d9259767fd8
KIPROTYCH <kiprotych@gmail.com>
2023-05-21 19:42:19 +0300

    created a match function

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const card = document.querySelectorAll(".tiles")
 const front = document.querySelectorAll(".front")
+const container = document.querySelector('.container')
+const score = document.querySelector('.score span')
 
 
 /* create a function that rearrages the images randomly */
 
 shuffleImage()
+clicking()
 function shuffleImage() {
     card.forEach(c=>{
         const num = [...Array(card.length).keys()]
@@ -14,14 +17,58 @@ function shuffleImage() {
     })
 }
 
-function clicking() {
-    for (let i =0; i<card.length; i++) {
-        card[i].addEventListener("click",()=>{
-            front[i],classlist.add("flip")
+function clicking(){
+
+    for(let i =0; i<card.length; i++){
+
+
+        front[i].classList.add('show')
+
+        setInterval(() => {
+            front[i].classList.remove('show')
+        }, 2000);
+
+        card[i].addEventListener('click' ,()=>{
+
+            front[i].classList.add('flip')
+           const filppedCard = document.querySelectorAll('.flip')
+
+            if(filppedCard.length == 2){
+
+                container.style.pointerEvents ='none'
+                
+                setInterval(() => {
+                    
+                    container.style.pointerEvents ='all'
+                }, 1000);
+ 
+                match(filppedCard[0] , filppedCard[1])
+            }
         })
-
-
-    }   
+    }
 }
-    
 
+
+function match(cardOne , cardTwo){
+
+    if(cardOne.dataset.index == cardTwo.dataset.index){
+
+        score.innerHTML = parseInt(score.innerHTML) + 1
+       
+        cardOne.classList.remove('flip') 
+        cardTwo.classList.remove('flip') 
+
+
+        cardOne.classList.add('match')
+        cardTwo.classList.add('match')
+
+
+    }else{
+
+        setTimeout(() => {
+            
+            cardOne.classList.remove('flip') 
+            cardTwo.classList.remove('flip') 
+        }, 1000);
+    }
+}
