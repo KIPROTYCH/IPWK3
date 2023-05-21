f668848b473de85c80cbca01f9f22624d3011e56
KIPROTYCH <kiprotych@gmail.com>
2023-05-21 10:03:07 +0300

    edited clicking functions

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const card = document.querySelectorAll(".tiles")
+const front = document.querySelectorAll(".front")
+
+
+/* create a function that rearrages the images randomly */
 
 shuffleImage()
-function shuffleImage(){
+function shuffleImage() {
     card.forEach(c=>{
         const num = [...Array(card.length).keys()]
         const random = Math.floor(Math.random()*card.length)
@@ -9,3 +13,15 @@ function shuffleImage(){
         c.style.order = num[random]
     })
 }
+
+function clicking() {
+    for (let i =0; i<card.length; i++) {
+        card[i].addEventListener("click",()=>{
+            front[i],classlist.add("flip")
+        })
+
+
+    }   
+}
+    
+
